db8fa4eb0bf31cb53e011642ba6e2e3420432ec5
Adam Batkin <adam@batkin.net>
2021-11-14 09:30:00 -0500

    make aws client building a little cleaner

--- cmd/ec2/util/aws.go
+++ cmd/ec2/util/aws.go
@@ -21,41 +21,58 @@ type AwsOptions struct {
 
 func BuildAwsClient(awsOptions *AwsOptions) *ec2.Client {
 	cfg, err := config.LoadDefaultConfig(context.Background(), func(options *config.LoadOptions) error {
+		var err error
+
+		// AWS Profile
 		if awsOptions.Profile != "" {
 			options.SharedConfigProfile = awsOptions.Profile
 		}
+
+		// AWS Region
 		if awsOptions.Region != "" {
 			options.Region = awsOptions.Region
 		}
+
+		// HTTP Proxy
 		httpClient := http.NewBuildableClient()
-		if awsOptions.Timeout > 0 {
-			httpClient = httpClient.WithTimeout(awsOptions.Timeout)
-		}
 		if awsOptions.Proxy != "" {
-			if awsOptions.Proxy == "none" {
-				httpClient = httpClient.WithTransportOptions(func(transport *http2.Transport) {
-					transport.Proxy = func(request *http2.Request) (*url.URL, error) {
-						return nil, nil
-					}
-				})
-			} else {
-				proxyUrl, err := url.Parse(awsOptions.Proxy)
-				if err != nil {
-					return fmt.Errorf("bad proxy url %s: %v", awsOptions.Proxy, err)
-				}
-				httpClient = httpClient.WithTransportOptions(func(transport *http2.Transport) {
-					transport.Proxy = http2.ProxyURL(proxyUrl)
-				})
+			if httpClient, err = setupProxy(awsOptions.Proxy, httpClient); err != nil {
+				return err
 			}
 		}
 		options.HTTPClient = httpClient
+
 		return nil
 	})
+
 	if err != nil {
 		log.Fatalf("unable to load SDK config: %v", err)
 	}
 
 	client := ec2.NewFromConfig(cfg)
-
 	return client
+}
+
+func setupProxy(proxy string, httpClient *http.BuildableClient) (*http.BuildableClient, error) {
+	var proxyFunc func(*http2.Request) (*url.URL, error)
+	if proxy == "none" {
+		proxyFunc = forceNoProxy()
+	} else {
+		if proxyUrl, err := url.Parse(proxy); err != nil {
+			return nil, fmt.Errorf("bad proxy url %s: %v", proxy, err)
+		} else {
+			proxyFunc = http2.ProxyURL(proxyUrl)
+		}
+	}
+
+	httpClient = httpClient.WithTransportOptions(func(transport *http2.Transport) {
+		transport.Proxy = proxyFunc
+	})
+	return httpClient, nil
+}
+
+func forceNoProxy() func(request *http2.Request) (*url.URL, error) {
+	return func(request *http2.Request) (*url.URL, error) {
+		return nil, nil
+	}
 }
