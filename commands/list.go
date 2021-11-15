af85e1d29d63de14b31ea35a3488379de7e18607
Adam Batkin <adam@batkin.net>
2021-11-15 11:21:43 -0500

    use interface instead of complex struct to define a command

--- commands/list.go
+++ commands/list.go
@@ -7,30 +7,65 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/ec2"
 	"github.com/aws/aws-sdk-go-v2/service/ec2/types"
 	"github.com/spf13/cobra"
-	"log"
 	"strings"
 )
 
-type Options struct {
+type ListEc2InstancesCommand struct {
 	instanceIds   []string
 	filterOptions map[string]string
 }
 
-func buildEc2ListCommand() ec2Command {
-	listOptions := &Options{}
+func (c *ListEc2InstancesCommand) optionSetup(command *cobra.Command) {
+	// TODO implement this
+}
 
-	return ec2Command{
-		optionSetup: func(command *cobra.Command) {
-			// TODO: add cli options using listOptions
-		},
-		usage:             "list [--filter name=value ... | --instance-id id ...]",
-		description:       "List EC2 Instances",
-		command: func(awsOptions *utilAws.Options) ([]utilDisplay.Item, error) {
-			return listEc2(awsOptions, listOptions)
-		},
-		defaultFieldNames: []string{"id", "tags.Name", "state"},
-		header:            buildHeading,
+func (c *ListEc2InstancesCommand) usage() string {
+	return "list [--filter name=value ... | --instance-id id ...]"
+}
+
+func (c *ListEc2InstancesCommand) description() string {
+	return "List EC2 Instances"
+}
+
+func (c *ListEc2InstancesCommand) runCommand(awsOptions *utilAws.Options) ([]utilDisplay.Item, error) {
+	client := awsOptions.BuildAwsClient()
+	requestContext := awsOptions.BuildRequestContext()
+
+	instancesPaginator := ec2.NewDescribeInstancesPaginator(client, &ec2.DescribeInstancesInput{})
+	items := make([]utilDisplay.Item, 0)
+	for instancesPaginator.HasMorePages() {
+		page, err := instancesPaginator.NextPage(requestContext)
+		if err != nil {
+			return nil, err
+		}
+		for _, reservation := range page.Reservations {
+			for _, instance := range reservation.Instances {
+				items = append(items, newEc2Item(instance))
+			}
+		}
 	}
+	return items, nil
+}
+
+func (c *ListEc2InstancesCommand) defaultFieldNames() []string {
+	return []string{"id", "tags.Name", "state"}
+}
+
+func (c *ListEc2InstancesCommand) headerName(fieldName string) string {
+	if strings.Index(fieldName, "Tags.") == 0 {
+		return fieldName[5:]
+	} else if fieldName == "id" {
+		return "InstanceId"
+	} else if len(fieldName) >= 2 {
+		return strings.ToUpper(fieldName[0:1]) + fieldName[1:]
+	} else {
+		return fieldName
+	}
+}
+
+func buildEc2ListCommand() Ec2Command {
+	listOptions := &ListEc2InstancesCommand{}
+	return listOptions
 }
 
 type Ec2Item struct {
@@ -60,37 +95,5 @@ func (e *Ec2Item) GetValue(name string) string {
 		return string(e.State.Name)
 	default:
 		return ""
-	}
-}
-
-func listEc2(awsOptions *utilAws.Options, listOptions *Options) ([]utilDisplay.Item, error) {
-	client := awsOptions.BuildAwsClient()
-	requestContext := awsOptions.BuildRequestContext()
-
-	instancesPaginator := ec2.NewDescribeInstancesPaginator(client, &ec2.DescribeInstancesInput{})
-	items := make([]utilDisplay.Item, 0)
-	for instancesPaginator.HasMorePages() {
-		page, err := instancesPaginator.NextPage(requestContext)
-		if err != nil {
-			log.Fatalf("error in pagination: %v", err)
-		}
-		for _, reservation := range page.Reservations {
-			for _, instance := range reservation.Instances {
-				items = append(items, newEc2Item(instance))
-			}
-		}
-	}
-	return items, nil
-}
-
-func buildHeading(name string) string {
-	if strings.Index(name, "Tags.") == 0 {
-		return name[5:]
-	} else if name == "id" {
-		return "InstanceId"
-	} else if len(name) >= 2 {
-		return strings.ToUpper(name[0:1]) + name[1:]
-	} else {
-		return name
 	}
 }
--- commands/shared.go
+++ commands/shared.go
@@ -7,34 +7,31 @@ import (
 	"log"
 )
 
-type optionSetupFunction func(command *cobra.Command)
-type commandFunction func(awsOptions *utilAws.Options) ([]display.Item, error)
-
-type ec2Command struct {
-	optionSetup optionSetupFunction
-	usage string
-	description string
-	command commandFunction
-	defaultFieldNames []string
-	header display.HeaderFunction
+type Ec2Command interface {
+	optionSetup(command *cobra.Command)
+	usage() string
+	description() string
+	runCommand(awsOptions *utilAws.Options) ([]display.Item, error)
+	defaultFieldNames() []string
+	headerName(field string) string
 }
 
-func setupCommand(command ec2Command, awsOptions *utilAws.Options, displayOptions *display.Options) *cobra.Command {
+func setupCommand(command Ec2Command, awsOptions *utilAws.Options, displayOptions *display.Options) *cobra.Command {
 	cobraCommand := &cobra.Command{
-		Use: command.usage,
-		Short: command.description,
+		Use: command.usage(),
+		Short: command.description(),
 		DisableFlagsInUseLine: true,
 		Run: func(cmd *cobra.Command, args []string) {
-			if result, err := command.command(awsOptions); err != nil {
+			if result, err := command.runCommand(awsOptions); err != nil {
 				log.Fatalf("error running command: %v", err)
 			} else {
 				var fields []string
 				if len(displayOptions.Fields) > 0 {
 					fields = displayOptions.Fields
 				} else {
-					fields = command.defaultFieldNames
+					fields = command.defaultFieldNames()
 				}
-				displayOptions.Render(fields, command.header, result)
+				displayOptions.Render(fields, command.headerName, result)
 			}
 		},
 	}
